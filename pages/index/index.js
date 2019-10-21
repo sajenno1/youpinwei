b0581ee62309e5bc0b7bf5908b249085bb79cea0
WSJ <13799597982@qq.com>
2019-10-21 17:31:41 +0800

    none

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,54 +1,128 @@
 //index.js
 //获取应用实例
 const app = getApp()
-
+const iconHttp = "http://abtl.mooshine.cn/Media/Abtl/AdvertItemPhotos/"
+const imgHttp ="http://ypw.sunton.cn/Media/Default/ProductPhotos/"
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
+    advert: [
+      '../../image/swiper1.jpg', 
+      '../../image/swiper2.jpg', 
+      '../../image/swiper3.jpg'
+    ],
+    menus: [
+      {
+        title:'新品促销', 
+        url: '', 
+        imgUrl: `${iconHttp}/181116_210846c008174d9ebc0ba2e760cfca52.png`
+      },
+      {
+        title: '促销商品',
+        url: '',
+        imgUrl: `${iconHttp}/181116_ed61ac66cadc4eea80e33f488513b4aa.png` 
+      },
+      {
+        title: '排行榜',
+        url: '',
+        imgUrl: `${iconHttp}/181116_0604e5234b734affb8b70079312b7f75.png`
+      },
+      {
+        title: '已购商品',
+        url: '',
+        imgUrl: `${iconHttp}/181116_0be312e3a47f4c4095e90b0c958189ce.png`
       }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
+    ],
+    items: {
+      news: {
+        title: '新品首发',
+        moreUrl: '',
+        products: [
+          {
+            img: `${imgHttp}/190912_36784787c47242b4bf162d0e14ca7e76.jpg`,
+            productName: '纯石墨烯养护鼻罩',
+            productUrl: '',
+            productPrice: '298.5',
+            productDiscount: '7.5折',
+            productStock: '992'
+          },
+          {
+            img: `${imgHttp}/190912_36784787c47242b4bf162d0e14ca7e76.jpg`,
+            productName: '纯石墨烯养护鼻罩',
+            productUrl: '',
+            productPrice: '298.5',
+            productDiscount: '7.5折',
+            productStock: '992'
+          },
+          {
+            img: `${imgHttp}/190912_36784787c47242b4bf162d0e14ca7e76.jpg`,
+            productName: '纯石墨烯养护鼻罩',
+            productUrl: '',
+            productPrice: '298.5',
+            productDiscount: '7.5折',
+            productStock: '992'
+          }
+        ]
+      },
+      promotion: {
+        title: '促销商品',
+        moreUrl: '',
+        products: [
+          {
+            img: `${imgHttp}/190912_36784787c47242b4bf162d0e14ca7e76.jpg`,
+            productName: '纯石墨烯养护鼻罩',
+            productUrl: '',
+            productPrice: '298.5',
+            productDiscount: '7.5折',
+            productStock: '992'
+          },
+          {
+            img: `${imgHttp}/190912_36784787c47242b4bf162d0e14ca7e76.jpg`,
+            productName: '纯石墨烯养护鼻罩',
+            productUrl: '',
+            productPrice: '298.5',
+            productDiscount: '7.5折',
+            productStock: '992'
+          },
+          {
+            img: `${imgHttp}/190912_36784787c47242b4bf162d0e14ca7e76.jpg`,
+            productName: '纯石墨烯养护鼻罩',
+            productUrl: '',
+            productPrice: '298.5',
+            productDiscount: '7.5折',
+            productStock: '992'
+          }
+        ]
+      },
+      discount: {
+        title: '最新优惠',
+        moreUrl: '',
+        products: [
+          {
+            img: `${imgHttp}/190912_36784787c47242b4bf162d0e14ca7e76.jpg`,
+            productName: '纯石墨烯养护鼻罩',
+            productUrl: '',
+            productPrice: '298.5',
+            productDiscount: '7.5折',
+            productStock: '992'
+          },
+          {
+            img: `${imgHttp}/190912_36784787c47242b4bf162d0e14ca7e76.jpg`,
+            productName: '纯石墨烯养护鼻罩',
+            productUrl: '',
+            productPrice: '298.5',
+            productDiscount: '7.5折',
+            productStock: '992'
+          },
+          {
+            img: `${imgHttp}/190912_36784787c47242b4bf162d0e14ca7e76.jpg`,
+            productName: '纯石墨烯养护鼻罩',
+            productUrl: '',
+            productPrice: '298.5',
+            productDiscount: '7.5折',
+            productStock: '992'
+          }
+        ]
+      }
     }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
   }
 })
